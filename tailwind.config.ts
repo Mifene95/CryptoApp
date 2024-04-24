43ecf864429490b3154ee71044aa49f66e37f42d
Michael Fernandez <mifene95@gmail.com>
2024-04-24 17:54:57 +0100

    added light colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,13 +11,24 @@ const config: Config = {
   theme: {
     colors: {
       light: {
-        blue: "#1fb6ff",
-        text: "black"
+        background: "#F3F5F9",
+        american_blue: "#353570",
+        stargate: "#7878FA",
+        exodus: "#6161D6",
+        hera_blue: "#7474F2",
+        lavender: "#9D62D9",
+        orchid: "#00B1A7",
+        peevish_red: "#FE2264",
+        pink: "#D878FA",
+        orange: "#F7931A",
+        bar_blue: "#849DFF",
+        lavender_blue: "#CCCFA",
+        text1: "#232336",
+        text2: "#424286",
+        text3: "#D1D1D1",
+        text4: "#FFFFFF",
       },
-      dark:{
-        orange: "#ff7849",
-        text: "white"
-      },
+      dark: {},
     },
     spacing: {},
     fontSize: {},
