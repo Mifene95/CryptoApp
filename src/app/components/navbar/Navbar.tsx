6198949f5e7224adab31efeca455eb8151d6f272
Michael Fernandez <mifene95@gmail.com>
2024-04-26 17:28:14 +0100

    Added light theme

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Home from "./home/Home";
 import Portfolio from "./portfolio/Portfolio";
 import Search from "./search/Search";
 import DropDown from "./dropdown/DropDown";
-
+import Theme from "./theme/Theme";
 export default function Navbar() {
   return (
     <div className="flex flex-row justify-between">
@@ -17,6 +17,7 @@ export default function Navbar() {
           <Search />
         </div>
         <DropDown />
+        <Theme />
       </div>
     </div>
   );
--- src/app/components/navbar/theme/Theme.tsx
+++ src/app/components/navbar/theme/Theme.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Theme() {
-  return <div></div>;
+  return (
+    <div className="px-3 py-3">
+      <Image
+        src="/logo_theme.svg"
+        width={24}
+        height={24}
+        alt="Light mode logo"
+      />
+    </div>
+  );
 }
