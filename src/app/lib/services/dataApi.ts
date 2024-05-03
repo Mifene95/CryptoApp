b77e2152ef80222b29f01054dd108e406b2cb2a2
Michael <mifene95@gmail.com>
2024-05-03 07:20:57 +0100

    Added rtk-query setup

---
+++ src/app/lib/services/dataApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+type Data = {
+  data: {
+    markets: number;
+    active_cryptocurrencies: number;
+    total_market_cap: number;
+    total_volume: number;
+    market_cap_percentage: number;
+  };
+};
+
+export const dataApi = createApi({
+  reducerPath: "dataAPI",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.coingecko.com/api/v3/" }),
+  endpoints: (builder) => ({
+    getData: builder.query<Data, null>({
+      query: () => "global",
+    }),
+  }),
+});
+
+export const { useGetDataQuery } = dataApi;
