ffbbc4ce7144ff4bb4c2972063f78af3323eea24
Michael <mifene95@gmail.com>
2024-05-18 11:35:46 +0100

    Implemented fetchedData to Carousel

--- src/app/lib/services/coinApi.ts
+++ src/app/lib/services/coinApi.ts
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 type CoinData = {
   id: string;
+  image: string;
   symbol: string;
   name: string;
   current_price: number;
-  market_cap: number;
-  total_volume: number;
+  price_change_percentage_24h: number;
 };
 
 type MarketDataQueryParams = {
@@ -22,7 +22,12 @@ export const coinApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.coingecko.com/api/v3/" }),
   endpoints: (builder) => ({
     getMarketData: builder.query<CoinData[], MarketDataQueryParams>({
-      query: ({ vs_currency = "usd", order = "market_cap_desc", per_page = 20, page = 1 }) => 
+      query: ({
+        vs_currency = "usd",
+        order = "market_cap_desc",
+        per_page = 50,
+        page = 1,
+      }) =>
         `coins/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}&sparkline=false`,
     }),
   }),
