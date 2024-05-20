64012646e9cdb774424f1fe39ba5577b99e009df
Michael <mifene95@gmail.com>
2024-05-20 06:49:46 +0100

    Added responsive carousel

--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -48,7 +48,34 @@ function MultipleItems() {
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow onClick={() => {}} />,
     prevArrow: <SamplePrevArrow onClick={() => {}} />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   const extractData = data?.map((coin) => ({
     id: coin.id,
     image: coin.image,
