82aeacf634167c0a230c7fe1ef4d514e8e47c5ab
unknown <mifene95@gmail.com>
2024-06-11 08:15:15 +0100

    Update reduxStore with chartdata

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -4,15 +4,17 @@ import themeReducer from "./features/theme.slice";
 import { dataApi } from "./services/dataApi";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { coinApi } from "./services/coinApi";
+import { marketChartApi } from "./services/marketChartApi";
 
 export const store = configureStore({
   reducer: {
     themeReducer,
     [dataApi.reducerPath]: dataApi.reducer,
     [coinApi.reducerPath]: coinApi.reducer,
+    [marketChartApi.reducerPath]: marketChartApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([dataApi.middleware, coinApi.middleware]),
+    getDefaultMiddleware().concat([dataApi.middleware, coinApi.middleware, marketChartApi.middleware]),
 });
 
 setupListeners(store.dispatch);
