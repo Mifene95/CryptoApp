d1d1f847e40e13050c25e045a17ba38c76604c73
Michael <mifene95@gmail.com>
2024-07-10 16:55:32 +0100

    Added darkmode style to time selector

--- src/app/components/charts/timeStamps.tsx
+++ src/app/components/charts/timeStamps.tsx
@@ -1,15 +1,27 @@
 export default function TimeStamps() {
   return (
-    <div className="flex flex-row gap-2 bg-light-text1 rounded-md text-light-text4 w-[436px] h-[42px] items-center">
-      <button className="px-2 py-5 bg-dark-exodus rounded-md w-14 h-8 border my-1 ml-1">
+    <div className="flex flex-row gap-2 bg-light-text1 rounded-md text-light-text4 w-[436px] h-[42px] items-center font-font1 font-medium text-sm">
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate my-1 ml-1">
         1D
       </button>
-      <button>7D</button>
-      <button>14D</button>
-      <button>1M</button>
-      <button>1W</button>
-      <button>1Y</button>
-      <button>5Y</button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate">
+        7D
+      </button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate">
+        14D
+      </button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate">
+        1M
+      </button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate">
+        1W
+      </button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate">
+        1Y
+      </button>
+      <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate mr-1">
+        5Y
+      </button>
     </div>
   );
 }
