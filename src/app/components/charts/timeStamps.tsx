9b7bc673fb40c9e6b88a610c72c88bc3f4e994a6
Michael <mifene95@gmail.com>
2024-07-13 09:39:35 +0100

    Added dark/light styles to timestamps

--- src/app/components/charts/timeStamps.tsx
+++ src/app/components/charts/timeStamps.tsx
@@ -1,6 +1,6 @@
 export default function TimeStamps() {
   return (
-    <div className="flex flex-row gap-2 bg-light-text1 rounded-md text-light-text4 w-[436px] h-[42px] items-center font-font1 font-medium text-sm">
+    <div className="flex flex-row gap-2 dark:bg-light-text1 bg-light-lavender_blue rounded-md text-light-text4 w-[436px] h-[42px] items-center font-font1 font-medium text-sm">
       <button className="px-2 py-5 w-14 hover:bg-dark-exodus rounded-md border-dark-stargate my-1 ml-1">
         1D
       </button>
